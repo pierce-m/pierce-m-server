a489b42b741ffa396d70114063a25e02abe421d6
Pierce McEntagart <pierce.mt92@gmail.com>
2015-01-05 19:40:51 -0800

    Got ring server to work, waiting on hardware.

--- server.go
+++ server.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os/exec"
 )
 
 func ring_handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	ioutil.WriteFile("infile", []byte(r.FormValue("program")), 0644)
+	resp, err := exec.Command("ring", "infile").Output()
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+	} else {
+		fmt.Fprintf(w, "%s", string(resp))
+	}
 }
 
 func main() {
-	http.HandleFunc("/ring", handler)
+	http.HandleFunc("/ring/", ring_handler)
 	http.ListenAndServe(":9090", nil)
 }
